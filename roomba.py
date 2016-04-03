e9d29e4a61fc00979121f3470a50e5d3043ceff2
Sebastian Nagel <sebastian.nagel@ncoding.at>
2016-04-03 10:38:43 +0200

    Change prints to python3 syntax, added docs

--- roomba.py
+++ roomba.py
@@ -1,15 +1,19 @@
 # Python script for roomba serial interface
 # This script is meant to be run in interactive mode:
 #   python -i roomba.py
+#
+# Note on Roomba Connector: TX on UART -> TXD 'serial output from roomba'
+
 import serial
 import sys
 import time
 
-port = "/dev/ttyUSB0"
-s = serial.Serial(port, 115200)
-print "Starting interface"
+s = serial.Serial("/dev/ttyUSB0", 115200)
+print("Power on roomba and press any key")
+input()
+print("Starting interface")
 s.write(bytearray([128])) 
-print "Roomba ready"
+print("Roomba is ready if it just beeped")
 
 def send(bs):
   s.write(bytearray(bs))
@@ -19,12 +23,12 @@ def safeMode():
   time.sleep(0.1)
 
 def clean():
-  print "Start cleaning"
+  print("Start cleaning")
   send([135])
 
 def playNote():
   safeMode()
-  print "Playing a note"
+  print("Playing a note")
   send([140, 0, 1, 62, 32])
   time.sleep(0.1)
   send([141, 0])
@@ -35,20 +39,21 @@ def noLED():
 
 def allLED():
   safeMode()
-  print "Lighting all LEDs"
+  print("Lighting all LEDs")
   send([139, 15, 0, 255])
 
 def setDayTime(day, hour, minute):
-  print "Setting day/time:", day, hour, ":", minute
+  print("Setting day/time:", day, hour, ":", minute)
   send([168, day, hour, minute]) 
 
 def scheduleEveryDay(hour, minute):
-  print "Scheduling every day at", hour, ":", minute
+  print("Scheduling every day at", hour, ":", minute)
   send([167, 127, hour, minute, hour, minute, hour, minute,
         hour, minute, hour, minute, hour, minute, hour, minute])
-  print "Note: schedule only active in passive mode"
+  print("Note: schedule only active in passive mode")
 
 def exit():
-  print "Exiting interface"
+  print("Exiting interface")
   send([173])
+  s.close()
   sys.exit()
